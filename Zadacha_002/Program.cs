dda4bc045da40acd96925b0df170f145dc710a81
Sergey Zhavoronkov <zhavoronkov-s@mail.ru>
2022-06-24 23:42:09 +0300

    выполнил каждое задание в отдельной папке

---
+++ Zadacha_002/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+
+Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber) 
+{
+ Console.WriteLine($"Максимальное число: {firstNumber}, минимальное число: {secondNumber}");
+}
+else if (firstNumber < secondNumber)
+{ 
+ Console.WriteLine($"Максимальное число: {secondNumber}, минимальное число: {firstNumber}");
+}
+else if (firstNumber == secondNumber )
+{
+Console.WriteLine("Максимального значения нет, числа равны");
+}
+else
+{ 
+ Console.WriteLine("Введите другие числа");
+}
---
+++ Zadacha_006/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+chekEvenNumber(number); //вызов метода
+//chekEvenNumber(54);
+//chekEvenNumber(53);
+void chekEvenNumber(int currentNumber)
+{
+    if (currentNumber % 2 == 0)
+    {
+        Console.WriteLine("Число является четным");
+    }
+    else 
+    {
+        Console.WriteLine("Число является нечетным");
+    }
+}
+//сделал через метод, но в данной ситуации он, как я понимаю, совсем не нужен
